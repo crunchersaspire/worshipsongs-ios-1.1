ac435dc87e346fbf027d8a4906c73b370f30d83b
Seenivasan Sankaran <seenivasan.sankaran@mcruncher.com>
2015-03-31 13:39:17 +0800

    #2 created Utilities service class and common constants class

---
+++ worshipsongs/worshipsongs/UtilitiesService.swift
@@ -0,0 +1,17 @@
+//
+//  UtilitiesService.swift
+//  worshipsongs
+//
+//  Created by Seenivasan Sankaran on 3/31/15.
+//  Copyright (c) 2015 mCruncher. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import SystemConfiguration
+
+class UtilitiesService  {
+    
+    
+}
---
+++ worshipsongs/worshipsongs/classes/common/CommonConstants.swift
@@ -0,0 +1,13 @@
+//
+//  CommonConstants.swift
+//  worshipsongs
+//
+//  Created by Seenivasan Sankaran on 3/31/15.
+//  Copyright (c) 2015 mCruncher. All rights reserved.
+//
+
+import Foundation
+
+public struct CommonConstants{
+    
+}
