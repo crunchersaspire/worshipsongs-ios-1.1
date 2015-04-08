bc0bbc82751b64b5a9f7565ad0c44b5d14359851
Seenivasan Sankaran <seenivasan.sankaran@mcruncher.com>
2015-04-08 11:35:19 +0800

    #6 Supported to display songs in both iPad and iPhone

--- worshipsongs/worshipsongs/classes/controllers/MasterViewController.swift
+++ worshipsongs/worshipsongs/classes/controllers/MasterViewController.swift
@@ -29,6 +29,7 @@ class MasterViewController: UITableViewController, UIPopoverPresentationControll
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Worship Songs"
+        self.addSearchBarButton()
         self.songData = SongDao.instance.getSongList()
         // Do any additional setup after loading the view, typically from a nib.
         //self.navigationItem.leftBarButtonItem = self.editButtonItem()
@@ -57,17 +58,22 @@ class MasterViewController: UITableViewController, UIPopoverPresentationControll
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = self.songData[indexPath.row] as Songs
-                let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
+        print("prepare for segue: \(segue.identifier)")
         if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad)
         {
+             print("iPad")
+                if segue.identifier == "showDetail" {
+                    println("Selected index path:\(self.tableView.indexPathForSelectedRow())")
+                    if let indexPath = self.tableView.indexPathForSelectedRow() {
+                        let object = self.songData[indexPath.row] as Songs
+                        let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+                        controller.detailItem = object
+                        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                        controller.navigationItem.leftItemsSupplementBackButton = true
+                    }
+                }
+            
+            println("showSettingsPopOver")
             if segue.identifier == "showSettingsPopOver"{
                 var popoverContent = self.storyboard?.instantiateViewControllerWithIdentifier("SettingsView") as UIViewController
                 var nav = UINavigationController(rootViewController: popoverContent)
@@ -78,15 +84,17 @@ class MasterViewController: UITableViewController, UIPopoverPresentationControll
                 self.presentViewController(nav, animated: true, completion: nil)
             }
         }
-        else
-        {
-            if segue.identifier == "pushSettingView"{
-                self.performSegueWithIdentifier("pushSettingView", sender: self)
+        if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone){
+            println("iPhone")
+            if (segue.identifier == "showDetailPhoneView") {
+                let indexPath: AnyObject! = sender
+                println("Selected index path:\(indexPath.row)")
+                let object = self.songData[indexPath.row] as Songs
+                print("Song object: \(object)")
+                var detailController = segue.destinationViewController as DetailViewController;
+                detailController.detailItem = object
             }
-
         }
-        
-        
     }
 
     // MARK: - Table View
@@ -106,6 +114,18 @@ class MasterViewController: UITableViewController, UIPopoverPresentationControll
         cell.textLabel!.text = object.title
         return cell
     }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone)
+        {
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            performSegueWithIdentifier("showDetailPhoneView", sender: indexPath)
+        }
+       
+    }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
@@ -117,9 +137,15 @@ class MasterViewController: UITableViewController, UIPopoverPresentationControll
         return .FullScreen
     }
     
+    
     func presentationController(controller: UIPresentationController!, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController! {
         println("hi")
         return UINavigationController(rootViewController: controller.presentedViewController)
     }
+    
+    func addSearchBarButton(){
+        self.navigationItem.setRightBarButtonItem(UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: "searchButtonItemClicked:"), animated: true)
+    }
+
 }
 
