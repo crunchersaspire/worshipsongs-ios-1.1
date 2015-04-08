03cae75080a22f4e6cfea57124673269ee69c37f
Seenivasan Sankaran <seenivasan.sankaran@mcruncher.com>
2015-04-08 11:59:57 +0800

    #8 Created the table view cell for the color settings

--- worshipsongs/worshipsongs/classes/controllers/ColorSettingsTableViewController.swift
+++ worshipsongs/worshipsongs/classes/controllers/ColorSettingsTableViewController.swift
@@ -43,60 +43,23 @@ class ColorSettingsTableViewController: UITableViewController {
         default: fatalError("Unknown number of sections")
         }
     }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    
+    
+    func getTableViewCell(labelText: String)  -> UITableViewCell{
+        var tableCell: UITableViewCell = UITableViewCell()
+        var cellLabel: UILabel = UILabel()
+        tableCell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        cellLabel = UILabel(frame: CGRectMake(10, 10, 250, 25))
+        cellLabel.text = labelText
+        cellLabel.font = getDefaultFont()
+        cellLabel = UILabel(frame: CGRectMake(260, 15, 10, 10))
+        let userSelectedPrimaryColorData  =  NSUserDefaults.standardUserDefaults().objectForKey("tamilFontColor") as? NSData
+        cellLabel.backgroundColor = UIColor.blackColor()
+        return tableCell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    func getDefaultFont() -> UIFont{
+        return UIFont(name: "HelveticaNeue", size: CGFloat(14))!
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
